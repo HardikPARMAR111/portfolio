bf51aa5583e705cefec5d005c5e4118459de3849
HardikPARMAR111 <parmarhardik0192@gmail.com>
2025-05-06 12:17:11 +0530

    changes

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -6,9 +7,9 @@ const Footer = () => {
       <footer className='flex justify-around bg-black text-white py-8 sticky bottom-0 z-50'>
         <div className='text-center'>Copyright C facebook | All Rights Reserved.</div>
         <ul className='flex gap-2 text-sm'>
-            <a href="/"><li className='text-sm'>home</li></a>
-            <a href="/about"><li className='text-sm'>about</li></a>
-            <a href="/contact"><li className='text-sm'>contact</li></a>
+            <Link href="/"><li className='text-sm'>home</li></Link>
+            <Link href="/about"><li className='text-sm'>about</li></Link>
+            <Link href="/contact"><li className='text-sm'>contact</li></Link>
         </ul>
       </footer>
     </div>
